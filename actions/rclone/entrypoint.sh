3feb4c48d489273560ae0ad9ea23f86a49866f38
Salvydas Lukosius <sall@w-ss.io>
2022-09-28 07:54:53 +0100

    modified:   rclone/Dockerfile 	modified:   rclone/action.yml 	modified:   rclone/entrypoint.sh

--- actions/rclone/entrypoint.sh
+++ actions/rclone/entrypoint.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
-set -e
+if [[ $DEBUG == "false" ]]; then
+  # Carry on, but do quit on errors
+  set -e
+else
+  # Verbose debugging
+  set -exuo pipefail xtrace
+  export LOG_LEVEL=debug
+  export ACTIONS_STEP_DEBUG=true
+fi
 
 CONFIG_FILE=$(rclone config file | grep 'rclone.conf' | awk '{print $1}')
 
 if [[ -n $RCLONE_CONF ]]; then
-  echo "$RCLONE_CONF" >"$CONFIG_FILE"
+  if [[ -w $CONFIG_FILE ]]; then
+    echo "$RCLONE_CONF" >"$CONFIG_FILE"
+  else
+    echo "Please check write permissions for the file:"
+    echo "$CONFIG_FILE"
+    exit 1
+  fi
+else
+  echo "The configuaration for the rclone is not set"
+  exit 1
 fi
 
-command rclone "$*"
+if [[ ! -x "$(command -v rclone)" ]]; then
+  echo 'Error: rclone failed to install or has non-executable permissions' >&2
+  exit 1
+fi
+
+bash -c "rclone $*"
