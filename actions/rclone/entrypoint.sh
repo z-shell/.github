6da28cfecd9c0c43c00408bf52c300a825ba3c48
Salvydas Lukosius <sall@w-ss.io>
2022-09-28 04:03:24 +0100

    modified:   Dockerfile 	modified:   action.yml 	modified:   entrypoint.sh

--- actions/rclone/entrypoint.sh
+++ actions/rclone/entrypoint.sh
@@ -4,11 +4,19 @@ set -e
 
 RCLONE_CONF_PATH="${HOME}/.config/rclone"
 
+if [[ -z $GITHUB_TOKEN ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
 if [[ -n $RCLONE_CONF ]]; then
   if [[ ! -d $RCLONE_CONF_PATH ]]; then
     mkdir -p "$RCLONE_CONF_PATH"
   fi
   echo "$RCLONE_CONF" >"${RCLONE_CONF_PATH}/rclone.conf"
+else
+  echo "Set the RCLONE_CONF env variable."
+  exit 1
 fi
 
 set -o xtrace
