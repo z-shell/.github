9b877b142e8d3581ee5e5796e07d3af6ed4c115b
Salvydas Lukosius <sall@w-ss.io>
2022-09-28 07:22:51 +0100

    modified:   rclone/Dockerfile 	modified:   rclone/entrypoint.sh

--- actions/rclone/entrypoint.sh
+++ actions/rclone/entrypoint.sh
@@ -3,10 +3,9 @@
 set -e
 
 CONFIG_FILE=$(rclone config file | grep 'rclone.conf' | awk '{print $1}')
-echo "$CONFIG_FILE"
 
 if [[ -n $RCLONE_CONF ]]; then
   echo "$RCLONE_CONF" >"$CONFIG_FILE"
 fi
 
-sh -c "rclone $*"
+command rclone "$*"
