218f22ca277f094948834ae79e4d5fdf8d5b6cf3
Salvydas Lukosius <sall@w-ss.io>
2022-09-28 06:18:05 +0100

    modified:   rclone/action.yml 	modified:   rclone/entrypoint.sh

--- actions/rclone/entrypoint.sh
+++ actions/rclone/entrypoint.sh
@@ -2,12 +2,29 @@
 
 set -e
 
+if [[ -n $RCLONE_CONF_PATH ]]; then
+  if [[ -d $RCLONE_CONF_PATH ]]; then
+    echo "$RCLONE_CONF_PATH"
+  else
+    mkdir -p "$RCLONE_CONF_PATH"
+    echo "$RCLONE_CONF_PATH"
+  fi
+else
+  echo "The RCLONE_CONF_PATH environment variable is empty"
+  exit 1
+fi
+
 if [[ -n $RCLONE_CONF ]]; then
-  [[ -d $RCLONE_CONF_PATH ]] || mkdir -p "$RCLONE_CONF_PATH"
-  echo "$RCLONE_CONF" >"${RCLONE_CONF_PATH}/rclone.conf"
+  echo "$RCLONE_CONF" >"$RCLONE_CONF_PATH/rclone.conf"
 else
   echo "The RCLONE_CONF environment variable is empty"
   exit 1
 fi
 
-bash -c "rclone $RUN"
+if [[ -n $RUN ]]; then
+  rclone=$(rclone "$RUN")
+  echo "::set-output name=rclone::$rclone"
+else
+  echo "The RUN environment variable is empty"
+  exit 1
+fi
